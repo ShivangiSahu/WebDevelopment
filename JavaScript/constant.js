98e049b3a68cf2267d9eb5be74a0202158ffbbcd
Shivangi <shivangisahu2492@gmail.com>
2020-04-12 15:07:27 +0530

    JS

---
+++ JavaScript/constant.js
@@ -0,0 +1,30 @@
+const pi = 3.14;
+console.log(pi);
+
+let name = 'harry potter';
+console.log(name);
+let age = '23';
+typeof age; 
+let option = true;
+let height; 
+console.log(height);
+console.log(option);
+let eyecolor = null;
+
+let shopping = [];
+shopping = ['paint','pencil','brush','eraser'];
+console.log(shopping);
+ let number = [1,2, 9,7];
+ console.log(number[0]+number[3]);
+ console.log(number.sort());
+ number.push(6);
+ console.log(number.sort());
+ let mixArray = ['hello', 1, 'harry', 0.2];
+ console.log(mixArray);
+ let pen = {
+ 	color: 'blue',
+ 	type: 'ballpoint',
+ 	cost : 20 
+ };
+ console.log(pen);
+ console.log(pen.color);
